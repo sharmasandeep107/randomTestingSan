50ab5deb4a8331b50de54c0228146958185f3802
sharma.sandeep@prophecy.io <sharma.sandeep@prophecy.io>
2025-10-09 11:33:20 +0000

    v2

--- pipelines/p1.py
+++ pipelines/p1.py
@@ -2,4 +2,8 @@ Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@g
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    pass
+    p1__constant_value_query = Task(
+        task_id = "p1__constant_value_query", 
+        component = "Model", 
+        modelName = "p1__constant_value_query"
+    )
