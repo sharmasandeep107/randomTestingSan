ced7ff9d6857e5fd801fae1c0b8b0e76d5c4f6ea
sharma.sandeep@prophecy.io <sharma.sandeep@prophecy.io>
2026-01-12 10:55:17 +0000

    v2

---
+++ pipelines/p1.py
@@ -0,0 +1,38 @@
+from prophecy_pipeline_sdk.graph import *
+from prophecy_pipeline_sdk.properties import *
+configuration = {"schema" : {"type" : "record", "fields" : []}}
+metainfo = PipelineGraphMetadata(
+    label = "p1",
+    version = 1,
+    configuration = configuration,
+    schedule = schedule,
+    sensor_schedule = sensor_schedule,
+)
+
+with PipelineGraph(id = "p1", metainfo = metainfo) as graph:
+    orchestrationsource_0 = PipelineProcess(
+        name = "OrchestrationSource_0",
+        metadata = PipelineNodeMetadata(phase = 0),
+        properties = GemProperties.SFTPSourceSpecProperties(
+          format = GemProperties.SFTPSourceSpecProperties.FixedWidthReadFormatProperties(schema = None),
+          properties = GemProperties.SFTPSourceSpecProperties.SFTPSourceInternalProperties(
+            fileOperationProperties = GemProperties.SFTPSourceSpecProperties.SourceFileOperationProperties(
+              fileLoadingType = "filepath",
+              includeFileNameColumn = True
+            ),
+            filePath = {
+              "type": "concat_operation",
+              "properties": {"elements" : [{"type" : "literal", "properties" : {"value" : "rwl"}}]}
+            }
+          ),
+          connector = {"kind" : "sftp", "properties" : {"id" : "sftp_1"}, "type" : "connector"}
+        ),
+        ports = Ports(inputs = [], outputs = [Port(name = "out")], is_custom_output_schema = False)
+    )
+    p1__reformat_1 = PipelineProcess(
+        name = "p1__reformat_1",
+        metadata = PipelineNodeMetadata(phase = 0),
+        properties = GemProperties.ModelTransformSpecProperties(modelName = "p1__reformat_1"),
+        ports = Ports(inputs = [Port(name = "in_0")], outputs = [Port(name = "out_0")], is_custom_output_schema = False)
+    )
+    orchestrationsource_0 >> p1__reformat_1
